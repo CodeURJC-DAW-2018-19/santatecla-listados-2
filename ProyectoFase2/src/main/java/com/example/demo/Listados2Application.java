33a199732b1e30799840d47790a54d794bfdf05b
Jaime <devo9070@gmail.com>
2019-02-11 12:38:37 +0100

    Project changed
    Changed structure to 2 projects for management quality

---
+++ ProyectoFase2/src/main/java/com/example/demo/Listados2Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Listados2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Listados2Application.class, args);
+    }
+
+}
+
