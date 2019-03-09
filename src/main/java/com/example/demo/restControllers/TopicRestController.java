e813ff7447089a04d9c463c25cf4ca8dbf5969e7
Haritz <haritzgamarra98@gmail.com>
2019-03-09 21:38:49 +0100

    First try of pagination

--- src/main/java/com/example/demo/restControllers/TopicRestController.java
+++ src/main/java/com/example/demo/restControllers/TopicRestController.java
@@ -5,6 +5,9 @@ import com.example.demo.topic.Topic;
 import com.example.demo.topic.TopicService;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +31,12 @@ public class TopicRestController {
     public ResponseEntity<List<Topic>> getTopics() {
         return new ResponseEntity<>(topicService.findAll(), HttpStatus.OK);
     }
+
+    /*      With this method we page th results of Showing all topics. It does not work properly
+    @GetMapping("/pag")
+    public Page<Topic> getTopics(Pageable page) {
+        return topicService.findAll(page);
+    }*/
 
     @JsonView(TopicDetails.class)
     @GetMapping("/{id}")
--- src/main/java/com/example/demo/topic/TopicRepository.java
+++ src/main/java/com/example/demo/topic/TopicRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.topic;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +11,7 @@ import java.util.Optional;
 public  interface  TopicRepository extends JpaRepository<Topic,Integer> {
     Optional<Topic> findByName(String name);
     List<Topic> findByNameContaining(String name);
+
+    //Page<Topic> findAll(Pageable page);
 }
 
