a708e881ab04d26c22a1fde21dfaae4be0be2f5e
Haritz <haritzgamarra98@gmail.com>
2019-03-09 04:21:00 +0100

    Added methods to StudentRestController using @RequestBody

--- src/main/java/com/example/demo/restControllers/StudentRestController.java
+++ src/main/java/com/example/demo/restControllers/StudentRestController.java
@@ -32,24 +32,6 @@ public class StudentRestController {
 
     //Question region
 
-
-    @JsonView(QuestionDetails.class)
-    @PostMapping("/{conceptName}/{questionText}/{questionType}/{corrected}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Question newConcreteQuestion(@PathVariable String conceptName, @PathVariable String questionText, @PathVariable String questionType, @PathVariable boolean corrected){
-        Question question = new Question();
-        Concept concept = conceptService.findOne(conceptName);
-        question.setQuestion(questionText);
-        question.setType(questionType);
-        question.setCorrected(corrected);
-        question.setConcept(concept);
-        concept.setQuestion(question);
-
-        conceptService.save(concept);
-
-        return question;
-    }
-
     @JsonView(QuestionDetails.class)
     @GetMapping("/questions/all")
     public List<Question> getQuestions(){
@@ -69,7 +51,66 @@ public class StudentRestController {
         }
     }
 
+    @JsonView(QuestionDetails.class)
+    @RequestMapping(value = "/newQuestion", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Question newQuestion(@RequestBody Question question) {
 
+        questionService.save(question);
+
+        return question;
+    }
+
+    @JsonView(QuestionDetails.class)
+    @RequestMapping(value = "/updateQuestion/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Question> updateQuestion(@PathVariable int id, @RequestBody Question updatedQuestion){
+        Question question = questionService.findOne(id);
+
+        if(question != null){
+            updatedQuestion.setId(id);
+            questionService.save(updatedQuestion);
+            return new ResponseEntity<>(updatedQuestion, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @JsonView(QuestionDetails.class)
+    @RequestMapping(value = "/question/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Question> deleteQuestion(@PathVariable int id){
+        Question question = questionService.findOne(id);
+
+        if (question != null){
+            questionService.delete(id);
+            return new ResponseEntity<>(question, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+        //New Question using UrRL parameters
+    @JsonView(QuestionDetails.class)
+    @PostMapping("/{conceptName}/{questionText}/{questionType}/{corrected}")
+    public ResponseEntity<Question> newConcreteQuestion(@PathVariable String conceptName, @PathVariable String questionText, @PathVariable String questionType, @PathVariable boolean corrected){
+        Concept concept = conceptService.findOne(conceptName);
+
+        if (concept != null) {
+            Question question = new Question();
+            question.setQuestion(questionText);
+            question.setType(questionType);
+            question.setCorrected(corrected);
+            question.setConcept(concept);
+            concept.setQuestion(question);
+
+            conceptService.save(concept);
+            return new ResponseEntity<>(question, HttpStatus.CREATED);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+        //Update Questions using  URL parameters
     @JsonView(QuestionDetails.class)
     @RequestMapping(value = "/question/updateText/{id}/{newText}", method = RequestMethod.PUT)
     public ResponseEntity<Question> updateQuestionText(@PathVariable int id, @PathVariable String newText){
@@ -98,37 +139,9 @@ public class StudentRestController {
         }
     }
 
-    @JsonView(QuestionDetails.class)
-    @RequestMapping(value = "/question/delete/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Question> deleteQuestion(@PathVariable int id){
-        Question question = questionService.findOne(id);
-
-        if (question != null){
-            questionService.delete(id);
-            return new ResponseEntity<>(question, HttpStatus.OK);
-        }else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
     // end region
 
     //Answer region
-
-    @JsonView(AnswerDetails.class)
-    @PostMapping("/question/{questionId}/newAnswer/{openAnswer}/{mark}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Answer newAnswer(@PathVariable int questionId, @PathVariable String openAnswer, @PathVariable boolean mark){
-        Answer answer = new Answer();
-        Question question = questionService.findOne(questionId);
-        answer.setOpenAnswer(openAnswer);
-        answer.setMark(mark);
-        answer.setQuestion(question);
-        question.setAnswer(answer);
-        questionService.save(question);
-
-        return answer;
-    }
 
     @JsonView(AnswerDetails.class)
     @GetMapping("/answers/all")
@@ -148,6 +161,69 @@ public class StudentRestController {
         }
     }
 
+    @JsonView(AnswerDetails.class)
+    @RequestMapping(value = "/newAnswer", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Answer newAnswer(@RequestBody Answer answer) {
+
+        answerService.save(answer);
+
+        return answer;
+    }
+
+    @JsonView(AnswerDetails.class)
+    @RequestMapping(value = "/updateAnswer/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Answer> updateAnswer(@PathVariable int id, @RequestBody Answer updatedAnswer){
+        Answer answer = answerService.findOne(id);
+
+       if(answer != null){
+           updatedAnswer.setId(id);
+           answerService.save(updatedAnswer);
+           return new ResponseEntity<>(updatedAnswer, HttpStatus.OK);
+       } else {
+           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+       }
+    }
+
+    @JsonView(AnswerDetails.class)
+    @RequestMapping(value = "/question{questionId}/deleteAnswer/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Answer> deleteAnswer(@PathVariable int id, @PathVariable int questionId) {
+        Question question = questionService.findOne(questionId);
+        Answer answer = answerService.findOne(id);
+        if (question != null) {
+            if (answer != null) {
+                question.setAnswer(null);
+                answerService.delete(id);
+                return new ResponseEntity<>(answer, HttpStatus.OK);
+            }else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+        //New Answers using UrRL parameters
+    @JsonView(AnswerDetails.class)
+    @PostMapping("/question/{questionId}/newAnswer/{openAnswer}/{mark}")
+    public ResponseEntity<Answer> newConcreteAnswer(@PathVariable int questionId, @PathVariable String openAnswer, @PathVariable boolean mark){
+
+        Question question = questionService.findOne(questionId);
+
+        if (question != null) {
+            Answer answer = new Answer();
+            answer.setOpenAnswer(openAnswer);
+            answer.setMark(mark);
+            answer.setQuestion(question);
+            question.setAnswer(answer);
+            questionService.save(question);
+            return new ResponseEntity<>(answer, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+        //Update Answers using  URL parameters
     @JsonView(AnswerDetails.class)
     @RequestMapping(value = "/answer/updateOpenAnswer/{id}/{openAnswer}", method = RequestMethod.PUT)
     public ResponseEntity<Answer> updateOpenAnswer(@PathVariable int id, @PathVariable String openAnswer){
@@ -176,20 +252,5 @@ public class StudentRestController {
         }
     }
 
-    @JsonView(AnswerDetails.class)
-    @RequestMapping(value = "/question{questionId}/deleteAnswer/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Answer> deleteAnswer(@PathVariable int id, @PathVariable int questionId) {
-        Question question = questionService.findOne(questionId);
-
-        if (question != null) {
-            question.setAnswer(null);
-            Answer answer = answerService.findOne(id);
-            answerService.delete(id);
-            return new ResponseEntity<>(answer, HttpStatus.OK);
-        }else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
     //end region
-
 }
