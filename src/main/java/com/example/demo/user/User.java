f7ca2d9ebff9451742693274ddfefc82faa70a6a
HaritzGamarra <haritzgamarra98@gmail.com>
2019-04-07 19:54:34 +0200

    Changes in backend. Fixed register method in UserRestController. Fully functional register and logIn (Angular)

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -66,7 +66,7 @@ public class User {
     }
 
     public void setPassword(String password) {
-        this.password =  new BCryptPasswordEncoder().encode(password);
+        this.password = password;
     }
 
 
